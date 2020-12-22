6ab35565966e6eee6386b9da753ffe0ed3e6421b
Israel Santizo <joseisraelss.jiss@gmail.com>
2020-12-21 20:44:31 -0600

    Se crea la accion que se realiza

---
+++ main.js
@@ -0,0 +1,9 @@
+//Variable for the tag a
+var anchor = document.getElementById('anchor');
+//Make a event (click) and function to do when make a click in the tag
+anchor.addEventListener('click', function(){
+    //Variable for h1
+    var name = document.getElementById('mr-js');
+    //what would you want to write when de even is true
+    name.innerHTML = 'Not good bah!';
+})
