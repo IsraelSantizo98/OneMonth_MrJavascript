40b6120f3ca340667b7ef694cf85a8d153f7f7ab
Israel Santizo <joseisraelss.jiss@gmail.com>
2020-12-26 14:16:45 -0600

    Proyecto terminado

--- main.js
+++ main.js
@@ -3,14 +3,15 @@ var anchor = document.getElementById('anchor');
 //Make a event (click) and function to do when make a click in the tag
 anchor.addEventListener('click', function(){
     //Variable for h1
-    var name = document.getElementById('mr-js');
     //what would you want to write when de even is true
+    var name = document.getElementById('mr-js');
     name.innerHTML = 'Not good bah!';
 })
 //To change the tag a 
-var a_tag = document.querySelector('a');
+var a_tag = document.getElementById('active');
 //a_tag.innerHTML = "HI";
 //click , and something happing
 a_tag.addEventListener('click', function(){
-    alert('Hello')
+    var name2 = document.getElementById('mr-js');
+    name2.innerHTML = "Soy Mr Javascript";
 })
